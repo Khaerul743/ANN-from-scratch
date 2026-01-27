91f422a7e79877934f35cce40a142620299125e6
Khaerul743 <khaerullutfi028@gmail.com>
2026-01-27 22:34:49 +0700

    setup weight, activations, dll.

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import numpy as np
+import pandas as pd
 
 
 class ArtificialNeuralNetwork:
-    pass
+    def __init__(self, x_train: np.ndarray, y_train: np.ndarray):
+        self.x_train = x_train
+        self.y_train = y_train
+
+        self.weights = {}
+        self.preactivations = {}
+        self.activations = {}
+
+        self.data_size: tuple = np.shape(x_train)
+
+    def relu(self, x):
+        return np.maximum(0, x)
+
+    def sigmoid(self, x):
+        return 1.0 / (1.0 + np.exp(-x))
+
+    def dense(self, size: int):
+        self.weights[len(self.weights) + 1] = np.random.randn(self.data_size[1], size)
+
+    def train(self):
+        self.weights[len(self.weights) + 1] = np.random.randn(
+            np.shape(list(self.weights.values())[-1])[1], 1
+        )
